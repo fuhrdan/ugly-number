73d49df2e988953c624136e934da0dadc316ca49
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-07-31 14:05:33 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//*****************************************************************************
+//** 263. Ugly Number  leetcode                                              **
+//** Simple, slow implementation at top, faster code below.      --Dan       **
+//*****************************************************************************
+
+bool isUgly(int n) {
+//    printf("%d / 2 == %d\n",n,n%2);
+//    printf("%d / 3 == %d\n",n,n%2);
+//    printf("%d / 5 == %d\n",n,n%2);
+    if(n <= 0) return false;
+/*
+    while (n % 2 == 0) n /= 2;
+    while (n % 3 == 0) n /= 3;
+    while (n % 5 == 0) n /= 5;
+*/
+    int factors[] = {2, 3, 5};
+    for (int i = 0; i < 3; ++i) {
+        while (n % factors[i] == 0) {
+            n /= factors[i];
+        }
+    }    
+    return n == 1;
+}
